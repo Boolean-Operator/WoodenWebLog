3b9d921a43949d3b2dffae82a10a85b9e38f67f6
Mark Graybill <marktgraybill@gmail.com>
2022-09-10 00:26:41 -0400

    add styling and links to Article Listing Page, NavBar and Footer

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
 
-module.exports = environment
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    Popper: ['popper.js', 'default'],
+  })
+);
+module.exports = environment;
