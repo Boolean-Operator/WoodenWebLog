3fab39a75dfe29f4bd2bb30c70da0144024d45e4
Mark Graybill <marktgraybill@gmail.com>
2022-09-12 13:23:17 -0400

    Add users show page with robot avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def avatar_for(user, options = { size: '300x300' })
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    robot_url = "https://robohash.org/#{hash}.png/bgset_any?size=#{size}"
+    image_tag(robot_url, alt: user.username, class: "rounded-circle shadow mx-auto d-block")
+
+  end
 end
