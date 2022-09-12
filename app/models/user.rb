b720f132803e23bcb1c0e5383d408395beacb5fa
Mark Graybill <marktgraybill@gmail.com>
2022-09-11 23:49:59 -0400

    add BCrypt gem, password_digest to User table and has_secure_password to user.model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  
   before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true,
@@ -9,5 +10,5 @@ class User < ApplicationRecord
             uniqueness: {case_sensitive: false},
             length:{ maximum: 105 },
             format: { with: VALID_EMAIL_REGEX }  
-  
+  has_secure_password
 end
