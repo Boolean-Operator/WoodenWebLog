3f999bf13e8462dc67fe9f85686eb7b3cf1a1301
Mark Graybill <marktgraybill@gmail.com>
2022-09-12 14:39:18 -0400

    add avatar and link to article author in article card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your information has been updated successfully"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'edit'
     end
