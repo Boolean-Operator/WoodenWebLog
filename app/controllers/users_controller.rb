d035a4c19d163722d6d973a49aa0a62555e1531d
Mark Graybill <marktgraybill@gmail.com>
2022-09-12 23:31:05 -0400

    add current_user and logged_in? helper method, updated nav bar-> logged_in? and logged in new signups, refactor user controller with set_user before action/method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
   end
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -13,11 +15,11 @@ class UsersController < ApplicationController
   end
 
   def edit 
-  @user = User.find(params[:id])
+  #@user = User.find(params[:id])
   end
   
   def update 
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your information has been updated successfully"
       redirect_to user_path(@user)
@@ -29,7 +31,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome the Wooden Web Log #{@user.username}, you have successfully signed up"
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome the Wooden Web Log #{@user.username}, you have successfully joined."
       redirect_to articles_path
     else
       render 'new'
@@ -38,6 +41,10 @@ class UsersController < ApplicationController
 
 
   private 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.required(:user).permit(:username, :email, :password)
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,17 @@ module ApplicationHelper
     size = options[:size]
     robot_url = "https://robohash.org/#{hash}.png/bgset_any?size=#{size}"
     image_tag(robot_url, alt: user.username, class: "rounded-circle shadow mx-auto d-block")
-
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+    #@current_user = @current_user|| User.find(session[:user_id]) if session[:user_id] 
+  end
+
+  def logged_in?
+    !!current_user
+    #returns a boolean
+  end
+
+
 end
