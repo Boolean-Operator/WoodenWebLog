872a16bb5a01c61463d2077608d0775bfb06f7cc
Mark Graybill <marktgraybill@gmail.com>
2022-09-17 17:49:27 -0400

    create migration to add article_categories table, create article_category.rb model & add many-to_many association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name,
             presence: true,
             uniqueness: true,
